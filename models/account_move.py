2bd0e7349514185d420e9e7600533c31fa97b894
Henry Navarro <henrymanuelnavarro@gmail.com>
2025-08-01 11:24:18 -0400

    [UPDATE] account_move

--- models/account_move.py
+++ models/account_move.py
@@ -46,59 +46,68 @@ class AccountMove(models.Model):
         for invoice in self:
             invoice.fiscal_check = bool(invoice.fp_serial_num)
 
+
+
+    @api.model
+    def get_max_sequence(self, sequence_list):
+        max_num = -1
+        max_sequence = None
+        for sequence in sequence_list:
+            numbers = "".join(re.findall(r"\d+", sequence or ""))
+            if numbers and int(numbers) > max_num:
+                max_num = int(numbers)
+                max_sequence = sequence
+        return max_sequence
+
+    @api.model
+    def get_next_sequence(self, sequence):
+        flag = True
+        sequence_elements = re.split(r"(\d+)", sequence or "")
+        sequence_elements.reverse()
+        sequence_numbers = len([
+            *filter(lambda e: e.isdigit(), sequence_elements)
+        ])
+        new_sequence = []
+        for element in sequence_elements:
+            if flag and element.isdigit():
+                sequence_numbers -= 1
+                element_len = len(element)
+                element = str(int(element) + 1).zfill(element_len)
+                flag = False
+                if element_len != len(element) and sequence_numbers:
+                    element = "".zfill(element_len)
+                    flag = True
+            new_sequence.append(element)
+        new_sequence.reverse()
+        return "".join(new_sequence)
+
+
+    def _get_domain_for_fiscal_check(self):
+        return [
+            ("fiscal_check", "=", True),
+            ("move_type", "=", self.move_type),
+            ("company_id", "=", self.company_id.id),
+            ("journal_id", "=", self.journal_id.id),
+        ]
+
     @api.onchange("fiscal_check")
     def onchange_fiscal_check(self):
-
-        def get_max_sequence(sequence_list):
-            max_num = -1
-            max_sequence = None
-            for sequence in sequence_list:
-                numbers = "".join(re.findall(r"\d+", sequence or ""))
-                if numbers and int(numbers) > max_num:
-                    max_num = int(numbers)
-                    max_sequence = sequence
-            return max_sequence
-
-        def get_next_sequence(sequence):
-            flag = True
-            sequence_elements = re.split(r"(\d+)", sequence or "")
-            sequence_elements.reverse()
-            sequence_numbers = len([
-                *filter(lambda e: e.isdigit(), sequence_elements)
-            ])
-            new_sequence = []
-            for element in sequence_elements:
-                if flag and element.isdigit():
-                    sequence_numbers -= 1
-                    element_len = len(element)
-                    element = str(int(element) + 1).zfill(element_len)
-                    flag = False
-                    if element_len != len(element) and sequence_numbers:
-                        element = "".zfill(element_len)
-                        flag = True
-                new_sequence.append(element)
-            new_sequence.reverse()
-            return "".join(new_sequence)
-
-        if self.fiscal_check and not self.fp_serial_num:
-            if self.move_type in CUSTOMER_DOCUMENTS:
-                moves = self.env["account.move"].search([
-                    ("fiscal_check", "=", True),
-                    ("move_type", "=", self.move_type),
-                    ("company_id", "=", self.company_id.id),
-                    ("journal_id", "=", self.journal_id.id),
-                ])
-
-                max_control_number = get_max_sequence(
-                    moves.mapped("control_number"))
-                max_fiscal_correlative = get_max_sequence(
-                    moves.mapped("fiscal_correlative"))
-                self.control_number = get_next_sequence(max_control_number)
-                self.fiscal_correlative = get_next_sequence(
-                    max_fiscal_correlative)
-        else:
+        if not(self.fiscal_check and self.move_type in CUSTOMER_DOCUMENTS) or self.fp_serial_num:
             self.control_number = None
             self.fiscal_correlative = None
+            return 
+        
+        moves = self.env["account.move"].search(self._get_domain_for_fiscal_check())
+
+        control_numbers = moves.mapped("control_number")
+        fiscal_correlatives = moves.mapped("fiscal_correlative")
+
+        max_control_number = self.get_max_sequence(control_numbers)
+        max_fiscal_correlative = self.get_max_sequence(fiscal_correlatives)
+        
+        self.control_number = self.get_next_sequence(max_control_number)
+        self.fiscal_correlative = self.get_next_sequence(max_fiscal_correlative)
+        return
 
     def get_payments_for_fiscal_machine(self):
         invoice_list = list()
